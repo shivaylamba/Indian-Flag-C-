57a95e28b4af923854ca5be2d14ca9313e364961
Shivay Lamba <shivaylamba@gmail.com>
2019-08-15 02:56:19 +0530

    Indian Flag using C++

---
+++ independence.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <windows.h>
+using namespace std;
+int main(){
+         SetConsoleTextAttribute (GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED |
+                                                            FOREGROUND_GREEN | 
+                                                            FOREGROUND_INTENSITY | 
+                                                            BACKGROUND_GREEN
+                          );
+    int a,b,c;
+    for (b=c=10; a="- FIGURE?, UMKC,XYZHello Folks,\TFy!QJu ROo TNn(ROo)SLq SLq ULo+UHs UJq TNn*RPn/QPbEWS_JSWQAIJO^\NBELPeHBFHT}TnALVlBLOFAkHFOuFETp\HCStHAUFAgcEAelclcn^r^r\\tZvYxXy\T|S~Pn SPm SOn TNn ULo0ULo#ULo-W\Hq!WFs XDt!"  [b+++21]; )
+    for(; a-- > 64 ; ) putchar ( ++c=='Z' ? c = c/ 9:33^b&1);
+    int i = 0;
+    cout<<"Happy Independence Day! Jai Hind! Vande Matram -- Shivay Lamba";
+    //Step 1: Compile and run it.
+    //Step 2: Capture screenshot of India Map
+    //Step 3: Copy string between printf brackets line no.8 and paste it on Linkedin with screenshot of your output. Happy Coding ! Jai Hind !🇮🇳
+    //Original Code credits : IOCCC.
+    //This code was modified and written on 14th August at 20:03pm, 2019 at India #LearnInspireGrow 🇮🇳
+        cin.get();
+
+	return 0;
+}
